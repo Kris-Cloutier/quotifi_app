65f49fdd6e5d1b8e2759a0c3a62a6b2e6daeac3d
Kris-Cloutier <kriscloutier@nerdydevs.com>
2020-01-16 00:54:35 -0600

    Updated settings.py

--- quotifi_app/settings.py
+++ quotifi_app/settings.py
@@ -74,9 +74,26 @@ WSGI_APPLICATION = 'quotifi_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'quotifi_db',
+        'USER': 'kris',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',   # Or an IP Address that your database is hosted on
+        'PORT': '3306',
+        #optional:
+        'OPTIONS': {
+            'charset' : 'utf8',
+            'use_unicode' : True,
+             'init_command': 'SET '
+                'storage_engine=INNODB,'
+                'character_set_connection=utf8,'
+                'collation_connection=utf8_bin'
+                #'sql_mode=STRICT_TRANS_TABLES,'    # see note below
+                #'SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED',
+        },
+        'TEST_CHARSET': 'utf8',
+        'TEST_COLLATION': 'utf8_general_ci',
     }
 }
 
